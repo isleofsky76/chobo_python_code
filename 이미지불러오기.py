418106385323d3b0bbd5c675f1dc7fec33a6c282
chobo <isleofsky76@gmail.com>
2024-03-14 15:02:49 +0900

    Update 이미지불러오기.py

--- 이미지불러오기.py
+++ 이미지불러오기.py
@@ -3,7 +3,7 @@ from PIL import Image
 # 이미지 열기
 img = Image.open(r"C:\Users\isleo\OneDrive\Desktop\1 컴퓨터배우기\이미지를 text로 변환하기\p1.jpg")
 
-# 
+# 결과출력하기
 print(img.format, img.size, img.mode)
 #결과 : JPEG (370, 220) RGB
 
